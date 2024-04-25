ca85d4d22d0eeb33f9202637871fd8592c18fc6f
Scott Feichter <scottfeichter@gmail.com>
2024-04-25 08:15:30 -0700

    extra push

--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -1,1 +1,5 @@
--- Your code here 
+-- Your code here
+INSERT INTO customers (customer_name, customer_phone ) VALUES
+( 'Rachel', 1111111111)
+
+UPDATE customers SET customer_points=6 WHERE customer_name='Rachel'
